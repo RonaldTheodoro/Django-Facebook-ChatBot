0098001fa7df70fa79abd9bdecaef4e54acdfe68
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-03-05 15:04:57 -0300

    add token verification

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,11 @@
 from django.views import generic
 from django.http.response import HttpResponse
-
+from django.conf import settings
 
 class SpotifyBotView(generic.View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World')
+        if self.request.GET.get(u'hub.verify_token') == settings.TOKEN_VERIFY:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
